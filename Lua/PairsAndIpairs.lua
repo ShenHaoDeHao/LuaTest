db69fbcc59f3cd36ca3798e62ca7cc84f2ecc2f4
ShenHao <616649431@qq.com>
2022-01-27 11:55:01 +0800

    20220127

--- Lua/PairsAndIpairs.lua
+++ Lua/PairsAndIpairs.lua
@@ -1,1 +1,12 @@
 print("Lua")
+local ipairsTable = {1,2,3,4,5,6,7,8,9,10}
+local pairsTable = {1,"2",3,["40"] = 9,5,"6",7,"8",9,"ten",["十一"] = 11}
+print "This is ipairs"
+for index, value in ipairs(pairsTable) do
+    print("ipairsTable["..index.."]=="..value)
+end
+
+print("That is pairs")
+for key, value in pairs(pairsTable) do
+    print("ipairsTable["..key.."]=="..value)
+end
